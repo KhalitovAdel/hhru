15fd51041ce63fa86810ad4b5024df65d85b6082
Adel Khalitov <adelkhalitov1@gmail.com>
2022-06-28 02:34:28 +0300

    fix(include-checker): add sinitize function to highlight characters

--- src/utils/string/include.checker.ts
+++ src/utils/string/include.checker.ts
@@ -2,10 +2,14 @@ export class IncludeChecker {
     private readonly substringsRegexp: RegExp[];
 
     constructor(substrings: string[]) {
-        this.substringsRegexp = substrings.map((s) => new RegExp(`\\b${s}\\b`, 'gi'));
+        this.substringsRegexp = substrings.map((s) => new RegExp(`\\b${IncludeChecker.sanitizeWord(s)}\\b`, 'gi'));
     }
 
     public isInclude(str: string): boolean {
         return this.substringsRegexp.some((r) => r.test(str));
+    }
+
+    private static sanitizeWord(str: string): string {
+        return str.replace(/\+/g, '\\+');
     }
 }
