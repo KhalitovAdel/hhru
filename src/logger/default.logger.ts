493ec8f3f214eab6caee377d8211ccdd2637b555
Adel Khalitov <adelkhalitov1@gmail.com>
2022-07-10 16:26:37 +0300

    feat(logging): add timestamp to logs

--- src/logger/default.logger.ts
+++ src/logger/default.logger.ts
@@ -1,5 +1,6 @@
 import * as winston from 'winston';
 
 export const defaultLogger = winston.createLogger({
+    format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
     transports: [new winston.transports.Console({ level: 'verbose' })],
 });
